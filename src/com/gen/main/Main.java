4f01a46ea05526b30e456e5c7a485f4f4d8f2302
rubinettiStefano <stefano.rubinetti@gmail.com>
2022-09-27 15:32:59 +0200

    aaa

---
+++ src/com/gen/main/Main.java
@@ -0,0 +1,13 @@
+package com.gen.main;
+import static com.gen.pi.Gino.input;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		System.out.println(input());
+
+	}
+
+}
---
+++ src/com/gen/pi/Gino.java
@@ -0,0 +1,12 @@
+package com.gen.pi;
+
+import java.util.Scanner;
+
+public class Gino
+{
+
+	public static String input()
+	{
+		return new Scanner(System.in).nextLine();
+	}
+}
